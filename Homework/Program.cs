2eab8722700b509e2b2da5ad58de222e300d5212
Tomáš Patloka <tpatloka@gmail.com>
2024-11-29 17:27:31 +0100

    mergnuto

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Zadej Jméno: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Zadej Přijmení: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Zade svůj věk: ");
+            string age = Console.ReadLine();
+            int ageInt = int.Parse(age);
+
+            Console.WriteLine("Zadej svoji výšku (cm): ");
+            string height = Console.ReadLine();
+            int heightInt = int.Parse(height);
+
+
+            Console.WriteLine("Zadej svoji váhu (kg): ");
+            string weight = Console.ReadLine();
+            int weightInt = int.Parse(weight);
+
+            //Console.WriteLine(firstName + "" + lastName + " je starý/á " + ageInt + "let. " + "Váží " + weightInt + "kg a je " + heightInt + " centimetrů vysoký/á. ");
+            Console.WriteLine($"{firstName} {lastName} je starý/á {ageInt} let. Váží {weightInt} kg a je {heightInt} centimetrů vysoký/á.");
+            
+            // Převod výšky na metry
+            float heightCm = heightInt / 100.0f;
+
+            // výpočet BMI
+            float bmi = weightInt / (heightCm * heightCm);
+
+            // Výpis BMI
+            //Console.WriteLine("BMI je: " + bmi);
+            
+            // Výpis BMI s jedním des. číslem
+            Console.WriteLine($"BMI je: {bmi:F1}");
+
+
+
+
+
         }
     }
 }
