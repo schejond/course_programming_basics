c69ec77d552c9f27cb13c5aeedcc303dcd3d2e6c
Petr <laisek@email.cz>
2024-11-30 21:32:29 +0100

    Proveden prvni krok ukolu _ input a output zadanych udaju

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("State your name.");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("State your surname.");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("State your age.");
+            string age_string = Console.ReadLine();
+            Int32.TryParse(age_string, out int age);
+
+            Console.WriteLine("State your weight.");
+            string weight_string = Console.ReadLine();
+            Int32.TryParse(weight_string, out int weight);
+
+            Console.WriteLine("State your height.");
+            string height_string = Console.ReadLine();
+            Int32.TryParse(height_string, out int height);
+
+
+            Console.WriteLine(name + " je starý(á) " + age_string + " let.Váží " + weight_string + " kilogramů a je " + height_string + " centimetrů vysoký / á.");
+
+
+
+
+
+
+
+            Console.WriteLine("Your name is " + name);
         }
     }
 }
