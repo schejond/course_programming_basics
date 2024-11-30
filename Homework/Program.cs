e9b21e92a1346d38c65ec94836d158b6ec98a080
Petr <laisek@email.cz>
2024-11-30 22:33:41 +0100

    Vytvoren kalkulator BMI a provedeny mirne upravy.

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,37 +1,56 @@
-﻿namespace Homework
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Homework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("State your name.");
-            string name = Console.ReadLine();
 
-            Console.WriteLine("State your surname.");
-            string surname = Console.ReadLine();
+            for (int i = 0; i < 2; i++)
+            {
+                //Input and Output of NAME, SURNAME, AGE, WEIGHT, HEIGHT
+                Console.WriteLine((i+1) + ". osoba");
+                Console.WriteLine("\nJmeno:");
+                string name = Console.ReadLine();
 
-            Console.WriteLine("State your age.");
-            string age_string = Console.ReadLine();
-            Int32.TryParse(age_string, out int age);
+                Console.WriteLine("\nPrijmeni:");
+                string surname = Console.ReadLine();
 
-            Console.WriteLine("State your weight.");
-            string weight_string = Console.ReadLine();
-            Int32.TryParse(weight_string, out int weight);
+                Console.WriteLine("\nVek:");
+                string age_string = Console.ReadLine();
+                Int32.TryParse(age_string, out int age);
 
-            Console.WriteLine("State your height.");
-            string height_string = Console.ReadLine();
-            Int32.TryParse(height_string, out int height);
+                Console.WriteLine("\nVaha (kg):");
+                string weight_string = Console.ReadLine();
+                Int32.TryParse(weight_string, out int weight);
 
-
-            Console.WriteLine(name + " je starý(á) " + age_string + " let.Váží " + weight_string + " kilogramů a je " + height_string + " centimetrů vysoký / á.");
-
-
-
-
+                Console.WriteLine("\nVyska (cm):");
+                string height_string = Console.ReadLine();
+                Int32.TryParse(height_string, out int height);
 
 
+                Console.WriteLine("\n" + name + " je starý(á) " + age_string + " let. Váží " + weight_string + " kilogramů a je " + height_string + " centimetrů vysoký/á.\n");
 
-            Console.WriteLine("Your name is " + name);
+
+                //BMI part
+
+                double height_meters = (double)height / 100;
+                double BMI = weight / Math.Pow(height_meters, 2);
+
+                Console.WriteLine("BMI = " + BMI + "\n");
+
+            }
+
+            
+
+
+
+
+
+
+
+            
         }
     }
 }
