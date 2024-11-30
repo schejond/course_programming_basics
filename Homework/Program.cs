b7e0268088b4fd21c581f01904ea1d8753896f7c
abdulada224 <abdulada224@gmail.com>
2024-11-30 23:19:56 +0100

    hotove cviceni4

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,10 +1,93 @@
-﻿namespace Homework
+﻿using System;
+using System.Reflection;
+
+namespace Homework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int vek;
+
+            string jmeno;
+
+            string prijmeni;
+
+            double vaha;
+
+            int vyska;
+
+            int vekB;
+
+            string jmenoB;
+
+            string prijmeniB;
+
+            double vahaB;
+
+            int vyskaB;
+
+
+            Console.Write("Věk první osoby: ");
+
+            vek = int.Parse(Console.ReadLine());
+
+            Console.Write("Jméno první osoby: ");
+            
+            jmeno = Console.ReadLine();
+
+            Console.Write("Příjmení první osoby: ");
+
+            prijmeni = Console.ReadLine();
+
+            Console.Write("Váha první osoby: ");
+
+            vaha = double.Parse(Console.ReadLine());
+
+            Console.Write("Výška první osoby: ");
+
+            vyska = int.Parse(Console.ReadLine());
+
+            Console.Write("Věk druhé osoby: ");
+
+            vekB = int.Parse(Console.ReadLine());
+
+            Console.Write("Jméno druhé osoby: ");
+
+            jmenoB = Console.ReadLine();
+
+            Console.Write("Příjmení druhé osoby: ");
+
+            prijmeniB = Console.ReadLine();
+
+            Console.Write("Váha druhé osoby: ");
+
+            vahaB = double.Parse(Console.ReadLine());
+
+            Console.Write("Výška druhé osoby: ");
+
+            vyskaB = int.Parse(Console.ReadLine());
+
+            double BMI = (vaha / (vyska * vyska)) * 10000;
+            double BMIB = (vahaB / (vyskaB * vyskaB)) * 10000;
+
+            Console.WriteLine(jmeno + " " + prijmeni + " je starý(á) " + vek + " let. Váží " + vaha + " kilogramů a je " + vyska + " centimetrů vysoký/á.");
+
+            Console.WriteLine("Body mass index (BMI) je: " + BMI);
+
+            Console.WriteLine(jmenoB + " " + prijmeniB + " je starý(á) " + vekB + " let. Váží " + vahaB + " kilogramů a je " + vyskaB + " centimetrů vysoký/á.");
+
+            Console.WriteLine("Body mass index (BMI) je: " + BMI);
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
