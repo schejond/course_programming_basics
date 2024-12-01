fd32f602200ee78c4c07f2b2f18515a30143de4c
Tomáš Patloka <tpatloka@gmail.com>
2024-12-01 20:23:37 +0100

    znovu jsem vytvořin hw4

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,42 +6,43 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Zadej Jméno: ");
-            string firstName = Console.ReadLine();
-
-            Console.WriteLine("Zadej Přijmení: ");
-            string lastName = Console.ReadLine();
-
-            Console.WriteLine("Zade svůj věk: ");
-            string age = Console.ReadLine();
-            int ageInt = int.Parse(age);
-
-            Console.WriteLine("Zadej svoji výšku (cm): ");
-            string height = Console.ReadLine();
-            int heightInt = int.Parse(height);
+            for (int run = 0; run < 2; run++)
+            {
 
 
-            Console.WriteLine("Zadej svoji váhu (kg): ");
-            string weight = Console.ReadLine();
-            int weightInt = int.Parse(weight);
+                Console.WriteLine("Zadej Jméno: ");
+                string firstName = Console.ReadLine();
 
-            //Console.WriteLine(firstName + "" + lastName + " je starý/á " + ageInt + "let. " + "Váží " + weightInt + "kg a je " + heightInt + " centimetrů vysoký/á. ");
-            Console.WriteLine($"{firstName} {lastName} je starý/á {ageInt} let. Váží {weightInt} kg a je {heightInt} centimetrů vysoký/á.");
-            
-            // Převod výšky na metry
-            float heightCm = heightInt / 100.0f;
+                Console.WriteLine("Zadej Přijmení: ");
+                string lastName = Console.ReadLine();
 
-            // výpočet BMI
-            float bmi = weightInt / (heightCm * heightCm);
+                Console.WriteLine("Zade svůj věk: ");
+                string age = Console.ReadLine();
+                int ageInt = int.Parse(age);
 
-            // Výpis BMI
-            //Console.WriteLine("BMI je: " + bmi);
-            
-            // Výpis BMI s jedním des. číslem
-            Console.WriteLine($"BMI je: {bmi:F1}");
+                Console.WriteLine("Zadej svoji výšku (cm): ");
+                string height = Console.ReadLine();
+                int heightInt = int.Parse(height);
 
 
+                Console.WriteLine("Zadej svoji váhu (kg): ");
+                string weight = Console.ReadLine();
+                int weightInt = int.Parse(weight);
 
+               
+                Console.WriteLine($"{firstName} {lastName} je starý/á {ageInt} let. Váží {weightInt} kg a je {heightInt} centimetrů vysoký/á.");
+
+                // Převod výšky na metry
+                float heightCm = heightInt / 100.0f;
+
+                // výpočet BMI
+                float bmi = weightInt / (heightCm * heightCm);
+
+                // Výpis BMI s jedním des. číslem
+                Console.WriteLine($"BMI je: {bmi:F1}");
+
+
+            }
 
 
         }
