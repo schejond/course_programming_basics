4f0395789ca693b4c7736233e96eb954ca3aa5f9
Petr <bojko.petr2006@gmail.com>
2024-12-03 20:48:21 +0100

    Dokoncen domaci ukol lekce5
    Ahoj, dokoncil jsem domaci ukol. Doufam ze je vse jak ma byt, pokud ne tak mi napis a pokusim se to zpravit. Mej se.

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -3,6 +3,7 @@
     internal class Program
     {
         static void Main(string[] args)
+<<<<<<< Updated upstream
         {   
             Console.Write("Prosím zadej svůj věk: ");
             string VekPrvnihoUzivatele = Console.ReadLine();
@@ -47,6 +48,77 @@
             double BmiIndex2 = VahaDruhehoUzivateleDouble / (VyskaDruhehoUzivateleDoubleConvert * VyskaDruhehoUzivateleDoubleConvert);
             double BmiIndex2Zaokrouhleno = Math.Round(BmiIndex2);
             Console.WriteLine("Tve BMI je: " + BmiIndex2Zaokrouhleno);
+=======
+        {
+            Console.WriteLine("Ahoj vítám tě v mém programu!");
+            string name = JmenoUzivatele();
+            int vek = Vek();
+            double vyska = VyskaodUzivatele();
+            double vaha = VahaUzivatele();
+            //string zprava = Zprava();
+            Console.WriteLine(name + " je starý(á) " + vek + " let. Váží " + vaha + " kilogramů a je " + vyska + " centimetrů vysoký(á).");
+
+            double height = vyska / 100;
+            double weight = vaha;
+            double bmi = CalculateBmiIndex(height, weight);
+            double BmiRounded = Math.Round(bmi);
+            Console.WriteLine("Vaše bmi je " + BmiRounded);
+
+            if (BmiRounded <= 15)
+            {
+                Console.WriteLine("Tohle není úplně zdravé, zkus přibrat");
+            }
+            else if (BmiRounded >= 15 && BmiRounded <= 45)
+            {
+                Console.WriteLine("Mas idealni BMI");
+            }
+            else
+            {
+                Console.WriteLine("Zkus zhubnout");
+            }
+
+>>>>>>> Stashed changes
         }
+        public static int Vek()
+        {
+            Console.Write("Prosim zadej svuj vek");
+            string VekUzivatele = Console.ReadLine();
+            int VekUzivateleInt = int.Parse(VekUzivatele);
+            return VekUzivateleInt;
+        }
+        public static double CalculateBmiIndex(double height, double weight)
+        {
+            var bmi = weight / (height * height);
+            return bmi;
+
+        }
+        public static double VyskaodUzivatele()
+        {
+            Console.Write("Prosim zadej svou vysku v cm ");
+            string vstupVyska = Console.ReadLine();
+            double VyskaDouble = double.Parse(vstupVyska);
+            return VyskaDouble;
+        }
+        public static string JmenoUzivatele()
+        {
+            Console.Write("Prosim zadej sve jmeno ");
+            string JmenoUzivatele = Console.ReadLine();
+            return JmenoUzivatele;
+        }
+        public static double VahaUzivatele()
+        {
+            Console.Write("Prosim zadej svou hmotnost ");
+            string VahaUzivatele = Console.ReadLine();
+            double VahaUzivateleDouble = double.Parse(VahaUzivatele);
+            return VahaUzivateleDouble;
+        }
+        /*public static string Zprava()
+        {
+            string zpravaProUzivatele = (JmenoUzivatele() + " je starý(á) " + Vek() + " let. Váží " + VahaUzivatele() + " kilogramů a je " + VyskaodUzivatele() + " centimetrů vysoký(á).");
+            Console.WriteLine(zpravaProUzivatele);
+            return zpravaProUzivatele;
+                }*/
+
+
     }
 }
