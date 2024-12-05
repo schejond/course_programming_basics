fd91604476aba4dff47dfd6fbdfd469b1730d94f
Petr <laisek@email.cz>
2024-12-05 03:24:57 +0100

    Program ma vystup na stejne urovni, jako predchozi HW. Vse by melo byt upraveno do funkci. Provedeny drobne i vetsi upravy. Zbyva dokoncit if statementy z HW5.

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -4,62 +4,114 @@ namespace Homework
 {
     internal class Program
     {
+        //Globalni promenne. Upravuji se na pocatku kazdeho cyklu.
+        public static string global_name = "";
+        public static string global_surname = "";
+        public static int global_age = 0;
+        public static int global_weight = 0;
+        public static float global_height = 0.0f;
+        public static float global_BMI = 0.0f;
+
         static void Main(string[] args)
         {
-            int people = UserInputWMessage_ToInt("Uvedte pocet uzivatelu");
-            ConfusingCycle(people);
-
             
+
+            int people = UserInputWMessage_ToInt("Uvedte pocet uzivatelu");
+            MainCycle(people);
+
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
         }
-        private static double CalculateBMI(int heightCM, int weight)
+        //Kalkulace BMI
+        public static float CalculateBMI(float heightCM, int weight)
         {
-            double height_meters = (double)heightCM / 100;
-            double BMI = weight / Math.Pow(height_meters, 2);
+            float height_meters = heightCM / 100;
+            float BMI = (float)weight / (float)Math.Pow(height_meters, 2);
             return BMI;
         }
-
-        private static string UserInputWMessage(string message)
+        //Precte userinput z konzole a ulozi jako String
+        public static string UserInputWMessage_ToString(string message)
         {
             Console.WriteLine(message);
             string user_input = Console.ReadLine();
             return user_input;
         }
-
-        private static int UserInputWMessage_ToInt (string message)
+        //Precte userinput z konzole a ulozi jako Int
+        public static int UserInputWMessage_ToInt (string message)
         {
-            Int32.TryParse(UserInputWMessage(message), out int converted_int);
+            Int32.TryParse(UserInputWMessage_ToString(message), out int converted_int);
             return converted_int;
         }
-
-        private static void ConfusingCycle(int number_of_cycles)
+        //Precte userinput z konzole a ulozi jako Float
+        public static float UserInputWMessage_ToFloat(string message)
+        {
+            float converted_float = (float)UserInputWMessage_ToInt(message);
+            return converted_float;
+        }
+        //Hlavni cyklus, ktery si pocita pocet osob, kterym pocitame BMI, obsahuje ostatni funkce v poradi, jak davaji smysl.
+        public static void MainCycle(int number_of_cycles)
         {
             for (int i = 0; i < number_of_cycles; i++)
             {
-                //Input and Output of NAME, SURNAME, AGE, WEIGHT, HEIGHT
                 Console.WriteLine((i + 1) + ". osoba");
 
-                string name = UserInputWMessage("\nJmeno:");
+                DataCollector();
+                DataExtractor();
 
-                string surname = UserInputWMessage("\nPrijmeni:");
-
-                int age = UserInputWMessage_ToInt("\nVek:");
-
-
-                int weight = UserInputWMessage_ToInt("\nVaha (kg):");
-
-
-                int height = UserInputWMessage_ToInt("\nVaha (kg):");
-
-
-                //Console.WriteLine("\n" + name + " je starý(á) " + age_string + " let. Váží " + weight_string + " kilogramů a je " + height_string + " centimetrů vysoký/á.\n");
-                Console.WriteLine("\n" + name + " je starý(á) " + age + " let. Váží " + weight + " kilogramů a je " + height + " centimetrů vysoký/á.\n");
-
-
-                Console.WriteLine("BMI = " + CalculateBMI(height, weight) + "\n");
+                
+                
 
             }
 
             
         }
+        //Ziska data o uzivateli z userinputu, dopocita a vlozi je do globalnich promennych
+        public static void DataCollector ()
+        {
+            string name = UserInputWMessage_ToString("\nJmeno:");
+
+            string surname = UserInputWMessage_ToString("\nPrijmeni:");
+
+            int age = UserInputWMessage_ToInt("\nVek:");
+
+            int weight = UserInputWMessage_ToInt("\nVaha (kg):");
+
+            float height = UserInputWMessage_ToFloat("\nVyska (cm):");
+
+            global_name = name;
+
+            global_surname = surname;
+
+            global_age = age;
+
+            global_weight = weight;
+
+            global_height = height;
+
+            global_BMI = CalculateBMI(global_height, global_weight);
+
+        }
+        public static void DataExtractor()
+        {
+            Console.WriteLine("\n" + global_name + " " + global_surname + " je starý(á) " + global_age + " let. Váží " + global_weight + " kilogramů a je " + global_height + " centimetrů vysoký/á.\n");
+
+
+            Console.WriteLine("BMI = " + global_BMI + "\n");
+        }
+        //public static int BMICategory()
+        //{
+        //    if (true)
+        //    {
+        //
+        //    }
+        //    else if (true)
+        //    {
+        //
+        //    }
+        //
+        //}
     }
+
 }
