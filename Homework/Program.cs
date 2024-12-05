0a81e18cb5317d235c1283aab96ff8af15a28137
Petr <laisek@email.cz>
2024-12-05 01:55:15 +0100

    Vse vlozeno do jednotlivych funkci

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -6,51 +6,8 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-
-            for (int i = 0; i < 2; i++)
-            {
-                //Input and Output of NAME, SURNAME, AGE, WEIGHT, HEIGHT
-                Console.WriteLine((i+1) + ". osoba");
-                Console.WriteLine("\nJmeno:");
-                string name = Console.ReadLine();
-
-                Console.WriteLine("\nPrijmeni:");
-                string surname = Console.ReadLine();
-
-                Console.WriteLine("\nVek:");
-                string age_string = Console.ReadLine();
-                Int32.TryParse(age_string, out int age);
-
-                Console.WriteLine("\nVaha (kg):");
-                string weight_string = Console.ReadLine();
-                Int32.TryParse(weight_string, out int weight);
-
-                Console.WriteLine("\nVyska (cm):");
-                string height_string = Console.ReadLine();
-                Int32.TryParse(height_string, out int height);
-
-
-                Console.WriteLine("\n" + name + " je starý(á) " + age_string + " let. Váží " + weight_string + " kilogramů a je " + height_string + " centimetrů vysoký/á.\n");
-
-
-                //BMI part
-
-                // double height_meters = (double)height / 100;
-                // double BMI = weight / Math.Pow(height_meters, 2);
-                //
-                // Console.WriteLine("BMI = " + BMI + "\n");
-
-                Console.WriteLine("BMI = " + CalculateBMI(height, weight) + "\n");
-
-            }
-
-            
-
-
-
-
-
-
+            int people = UserInputWMessage_ToInt("Uvedte pocet uzivatelu");
+            ConfusingCycle(people);
 
             
         }
@@ -59,6 +16,50 @@ namespace Homework
             double height_meters = (double)heightCM / 100;
             double BMI = weight / Math.Pow(height_meters, 2);
             return BMI;
+        }
+
+        private static string UserInputWMessage(string message)
+        {
+            Console.WriteLine(message);
+            string user_input = Console.ReadLine();
+            return user_input;
+        }
+
+        private static int UserInputWMessage_ToInt (string message)
+        {
+            Int32.TryParse(UserInputWMessage(message), out int converted_int);
+            return converted_int;
+        }
+
+        private static void ConfusingCycle(int number_of_cycles)
+        {
+            for (int i = 0; i < number_of_cycles; i++)
+            {
+                //Input and Output of NAME, SURNAME, AGE, WEIGHT, HEIGHT
+                Console.WriteLine((i + 1) + ". osoba");
+
+                string name = UserInputWMessage("\nJmeno:");
+
+                string surname = UserInputWMessage("\nPrijmeni:");
+
+                int age = UserInputWMessage_ToInt("\nVek:");
+
+
+                int weight = UserInputWMessage_ToInt("\nVaha (kg):");
+
+
+                int height = UserInputWMessage_ToInt("\nVaha (kg):");
+
+
+                //Console.WriteLine("\n" + name + " je starý(á) " + age_string + " let. Váží " + weight_string + " kilogramů a je " + height_string + " centimetrů vysoký/á.\n");
+                Console.WriteLine("\n" + name + " je starý(á) " + age + " let. Váží " + weight + " kilogramů a je " + height + " centimetrů vysoký/á.\n");
+
+
+                Console.WriteLine("BMI = " + CalculateBMI(height, weight) + "\n");
+
+            }
+
+            
         }
     }
 }
